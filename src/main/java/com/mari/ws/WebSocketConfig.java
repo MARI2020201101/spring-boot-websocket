aa2cd3fa2f177b97d3dc9ad742c82930c24e89a0
MARI <mari20201101@gmail.com>
2021-08-08 00:00:28 +0900

    Fix error

--- src/main/java/com/mari/ws/WebSocketConfig.java
+++ src/main/java/com/mari/ws/WebSocketConfig.java
@@ -2,12 +2,12 @@ package com.mari.ws;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
@@ -26,7 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket").withSockJS();
-    
+
     }
 
 }
